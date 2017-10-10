142765d34278c74f079964400f645512f2ddd0f4
«Devforlive» <e_mail_post_box@yahoo.com>
2017-10-10 18:24:33 +0300

    Far_Cry_4_gamecard

--- main.js
+++ main.js
@@ -1,32 +1,13 @@
-(function () {
-	'use strict';
-
-	const btns = document.querySelectorAll('.stuff__btn');
-
-	btns.forEach(function (btn) {
-		btn.addEventListener('click', function() {	// callback
-			alert('Btn clicked');
-// this - ссылка на btn parentNode - родительский узел (classList(DOMtokenList))
-			 if (this.parentNode.classList.contains('special-offer')) {		// метод contains (аргумент special-offer)
-				this.dispatchEvent(specialEvent);
-			 	alert('special-offer');
-			} else {
-				alert('Btn clicked');
-		}
-	});
+$(document).ready(function(){
+ 
+    $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+ 
+        $('ul.tabs li').removeClass('active');
+        $('.tab-content').removeClass('active');
+ 
+        $(this).addClass('active');
+        $("#"+tab_id).addClass('active');
+    });
+ 
 });
-// выборка всех элементов кнопок "special offer"
-const btns = document.querySelectorAll('.special-offer > .stuff__btn');	// CSS селектор: '.special-offer > .stuff__btn'
-
-btnsSpecial.forEach(function(btn) {
-	btn.addEventListener('click', function() {
-		alert('Special Offer');
-	});
-});
-
-
-const specialEvent = new Event ('special-event');	// добавили обработчик нового события
-
-console.log(specialEvent);
-
-
